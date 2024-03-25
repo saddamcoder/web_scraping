4eb16042719ba6bba186dab271b951c725ba1a11
saddam hossain <saddamcoder@saddams-macbook-pro.local>
2024-03-25 23:38:43 +0000

    data structure

--- ecommerce_product_pages.py
+++ ecommerce_product_pages.py
@@ -2,3 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 # this is ecommerce web scraping project
+
+class EcommerceProductPages:
+    def __init__(self
+                 , base_url : str
+                 , product_name : str
+                 , product_description : str
+                 , product_price : str
+                 , product_quantity : str
+                 , product_image_url : str
+                 , product_url : str
+                 ):
+        self.base_url = base_url
+        self.product_name = product_name
+        self.product_description = product_description
+        self.product_price = product_price
+
